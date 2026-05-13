653e8712c5f4c677adc64e12e248f66833fc5bc0
wasim6038 <anassaifi6038@gmail.com>
2026-05-13 21:56:36 +0530

    fix for mongodb atlas connection

--- models/user.model.js
+++ models/user.model.js
@@ -97,7 +97,7 @@ const userSchema = new mongoose.Schema(
 );
 
 // ─── Indexes ──────────────────────────────────────────────────────────────────
-userSchema.index({ email: 1 });
+// userSchema.index({ email: 1 });
 userSchema.index({ role: 1 });
 userSchema.index({ createdAt: -1 });
 
